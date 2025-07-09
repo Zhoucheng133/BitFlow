f44b213ded7ea7c595bcc5bdd024050d76c6a1fc
zhoucheng <zhoucheng133@outlook.com>
2025-07-09 21:11:27 +0800

    配置window_manager

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,59 @@
+import 'package:bit_flow/getx/theme_get.dart';
+import 'package:bit_flow/main_window.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:window_manager/window_manager.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+  WindowOptions windowOptions = WindowOptions(
+    size: Size(800, 600),
+    center: true,
+    backgroundColor: Colors.transparent,
+    skipTaskbar: false,
+    titleBarStyle: TitleBarStyle.hidden,
+  );
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+  Get.put(ThemeGet());
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final ThemeGet themeGet=Get.find();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    final Brightness brightness = MediaQuery.of(context).platformBrightness;
+    themeGet.darkMode.value=brightness==Brightness.dark;
+
+    return Obx(()=>
+      GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.orange,
+            brightness: themeGet.darkMode.value ? Brightness.dark : Brightness.light
+          ),
+          useMaterial3: true,
+          textTheme: GoogleFonts.notoSansScTextTheme(),
         ),
-      ),
+        home: Scaffold(
+          body: MainWindow()
+        ),
+      )
     );
   }
 }
---
+++ lib/main_window.dart
@@ -0,0 +1,86 @@
+import 'dart:io';
+
+import 'package:bit_flow/getx/theme_get.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+import 'package:window_manager/window_manager.dart';
+
+class MainWindow extends StatefulWidget {
+  const MainWindow({super.key});
+
+  @override
+  State<MainWindow> createState() => _MainWindowState();
+}
+
+class _MainWindowState extends State<MainWindow> with WindowListener {
+  @override
+  void initState() {
+    super.initState();
+    windowManager.addListener(this);
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    super.dispose();
+  }
+
+  bool isMax=false;
+
+  @override
+  void onWindowMaximize() {
+    super.onWindowMaximize();
+    setState(() {
+      isMax=true;
+    });
+  }
+
+  @override
+  void onWindowUnmaximize() {
+    super.onWindowUnmaximize();
+    setState(() {
+      isMax=false;
+    });
+  }
+
+  final ThemeGet themeGet=Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          height: 30,
+          child: Row(
+            children: [
+              Expanded(child: DragToMoveArea(child: Container())),
+              if(Platform.isMacOS) Obx(()=>
+                Row(
+                  children: [
+                    WindowCaptionButton.minimize(
+                      onPressed: windowManager.minimize,
+                      brightness: themeGet.darkMode.value ? Brightness.dark : Brightness.light,
+                    ),
+                    isMax ? WindowCaptionButton.unmaximize(
+                      onPressed: windowManager.unmaximize,
+                      brightness: themeGet.darkMode.value ? Brightness.dark : Brightness.light,
+                    ) : WindowCaptionButton.maximize(
+                      onPressed: windowManager.maximize,
+                      brightness: themeGet.darkMode.value ? Brightness.dark : Brightness.light,
+                    ),
+                    WindowCaptionButton.close(
+                      onPressed: windowManager.close,
+                      brightness: themeGet.darkMode.value ? Brightness.dark : Brightness.light,
+                    ),
+                  ]
+                )
+              )
+            ],
+          ),
+        ),
+        // TODO
+      ],
+    );
+  }
+}
