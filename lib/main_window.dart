4def0b029c88312efa12b36f4b65960e724a0c60
zhoucheng <zhoucheng133@outlook.com>
2025-07-10 21:21:34 +0800

    删除不需要的导入

--- lib/main_window.dart
+++ lib/main_window.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:bit_flow/getx/theme_get.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:window_manager/window_manager.dart';
 
 class MainWindow extends StatefulWidget {
