0d7f9312eaeca2acb892aa7d126610125c21038f
zhoucheng <zhoucheng133@outlook.com>
2025-07-11 20:31:57 +0800

    迁移下载器检测代码

--- lib/getx/store_get.dart
+++ lib/getx/store_get.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:bit_flow/components/add_store.dart';
 import 'package:bit_flow/components/dialogs.dart';
 import 'package:bit_flow/service/aria.dart';
@@ -5,31 +7,21 @@ import 'package:bit_flow/service/qbit.dart';
 import 'package:bit_flow/types/types.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class StoreGet extends GetxController{
   final AriaService ariaService=Get.find();
   final QbitService qbitService=Get.find();
   RxList<StoreItem> servers = <StoreItem>[].obs;
 
-  Future<bool> checkStore(StoreItem item) async {
-    switch (item.type) {
-      case StoreType.aria:
-        final version=await ariaService.getVersion(item);
-        if(version!=null){
-          return true;
-        }
-        return false;
-      case StoreType.qbit:
-        final version=await qbitService.getVersion(item);
-        if(version!=null){
-          return true;
-        }
-        return false;
-    }
+  Future<void> saveStore() async {
+    final prefs=await SharedPreferences.getInstance();
+    prefs.setString("store", jsonEncode(servers.map((item)=>item.toMap()).toList()));
   }
 
   void addStore(BuildContext context, {init=false}){
     StoreItem item=StoreItem(StoreType.aria, "", null, "");
+    bool load=false;
     void setVal(StoreItem val){
       item = StoreItem(val.type, val.url, val.type==StoreType.aria ? null : val.username, val.password);
     }
@@ -37,29 +29,41 @@ class StoreGet extends GetxController{
     showDialog(
       context: context, 
       barrierDismissible: false,
-      builder: (context)=>AlertDialog(
-        title: const Text('添加一个下载服务器'),
-        content: SizedBox(
-          width: 400,
-          child: AddStore(valCallback: setVal,)
-        ),
-        actions: [
-          TextButton(
-            onPressed: init ? null : () => Navigator.pop(context), 
-            child: const Text("取消")
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if(await checkStore(item)){
-                servers.add(item);
-                if(context.mounted) Navigator.pop(context);
-              }else{
-                if(context.mounted) showErrWarnDialog(context, "连接下载器失败", "请检查服务器地址和登录信息");
-              }
-            }, 
-            child: const Text("添加")
-          )
-        ],
+      builder: (context)=>StatefulBuilder(
+        builder: (context, setState)  {
+          return AlertDialog(
+            title: const Text('添加一个下载服务器'),
+            content: SizedBox(
+              width: 400,
+              child: AddStore(valCallback: setVal,)
+            ),
+            actions: [
+              TextButton(
+                onPressed: init ? null : () => Navigator.pop(context), 
+                child: const Text("取消")
+              ),
+              ElevatedButton(
+                onPressed: load ? null : () async {
+                  setState((){
+                    load=true;
+                  });
+                  bool checked=await item.checkItem();
+                  if(checked){
+                    servers.add(item);
+                    saveStore();
+                    if(context.mounted) Navigator.pop(context);
+                  }else{
+                    if(context.mounted) showErrWarnDialog(context, "连接下载器失败", "请检查服务器地址和登录信息");
+                  }
+                  setState((){
+                    load=false;
+                  });
+                }, 
+                child: const Text("添加")
+              )
+            ],
+          );
+        }
       )
     );
   }
--- lib/types/types.dart
+++ lib/types/types.dart
@@ -1,3 +1,8 @@
+import 'package:bit_flow/getx/store_get.dart';
+import 'package:bit_flow/service/aria.dart';
+import 'package:bit_flow/service/qbit.dart';
+import 'package:get/get.dart';
+
 enum StoreType{
   aria,
   qbit,
@@ -10,4 +15,34 @@ class StoreItem{
   late String? username;
   late String password;
   StoreItem(this.type, this.url, this.username, this.password);
+
+  final AriaService ariaService=Get.find();
+  final QbitService qbitService=Get.find();
+  final StoreGet storeGet=Get.find();
+
+  Map toMap(){
+    return {
+      "type": type.index,
+      "url": url,
+      "username": username??"",
+      "password": password,
+    };
+  }
+
+  Future<bool> checkItem() async {
+    switch (type) {
+      case StoreType.aria:
+        final version=await ariaService.getVersion(this);
+        if(version!=null){
+          return true;
+        }
+        return false;
+      case StoreType.qbit:
+        final version=await qbitService.getVersion(this);
+        if(version!=null){
+          return true;
+        }
+        return false;
+    }
+  }
 }
