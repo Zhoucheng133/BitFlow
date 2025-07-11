52213fbd80947f3ef468c156c35d899d07cf7a34
zhoucheng <zhoucheng133@outlook.com>
2025-07-11 19:40:39 +0800

    添加qbit类

---
+++ lib/service/qbit.dart
@@ -0,0 +1,8 @@
+import 'package:bit_flow/types/types.dart';
+import 'package:get/get.dart';
+
+class QbitService extends GetxController {
+  Future<String?> getVersion(StoreItem item) async {
+    return null;
+  }
+}
