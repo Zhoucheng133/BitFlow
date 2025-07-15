9b13dd75d8fdd18f012d3c91543c2be4c07111d7
zhoucheng <zhoucheng133@outlook.com>
2025-07-15 17:52:50 +0800

    添加已完成页面

--- lib/pages/finish.dart
+++ lib/pages/finish.dart
@@ -1,5 +1,8 @@
 import 'package:bit_flow/components/header.dart';
+import 'package:bit_flow/components/task_components/finish_task.dart';
+import 'package:bit_flow/getx/status_get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class FinishPage extends StatefulWidget {
   const FinishPage({super.key});
@@ -9,11 +12,22 @@ class FinishPage extends StatefulWidget {
 }
 
 class _FinishPageState extends State<FinishPage> {
+
+  final StatusGet statusGet=Get.find();
+  
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Header(name: '已完成')
+        Header(name: "下载中"),
+        Obx(()=>
+          Expanded(
+            child: ListView.builder(
+              itemCount: statusGet.tasks.length,
+              itemBuilder: (BuildContext context, int index)=>FinishTask(item: statusGet.tasks[index])
+            ),
+          )
+        )
       ],
     );
   }
