486eb438c0eeb5deb8fc3606ad50fe2465ae88d0
zhoucheng <zhoucheng133@outlook.com>
2025-07-17 22:01:12 +0800

    添加显示顺序参数

--- lib/getx/status_get.dart
+++ lib/getx/status_get.dart
@@ -1,3 +1,4 @@
+import 'package:bit_flow/types/store_item.dart';
 import 'package:bit_flow/types/task_item.dart';
 import 'package:get/get.dart';
 
@@ -5,6 +6,19 @@ enum Pages{
   active,
   finish,
   settings
+}
+
+enum OrderTypes{
+  // Aria默认情况下新的在后面
+
+  // 添加日期（新的在前）
+  addNew,
+  // 添加日期（新的在后）
+  addOld,
+  // 任务名称A-->Z
+  nameAZ,
+  // 任务名称Z-->A
+  nameZA
 }
 
 class StatusGet extends GetxController{
@@ -17,11 +31,51 @@ class StatusGet extends GetxController{
   RxList<TaskItem> activeTasks=<TaskItem>[].obs;
   RxList<TaskItem> finishedTask=<TaskItem>[].obs;
 
-  void makeTasks(List<TaskItem> tasks){
+  Rx<OrderTypes> activeOrder=OrderTypes.addNew.obs;
+  Rx<OrderTypes> finishOrder=OrderTypes.addOld.obs;
+
+  List<TaskItem> orderHandler(List<TaskItem> tasks, OrderTypes order, StoreType serverType){
+    switch (order) {
+      case OrderTypes.nameAZ:
+        tasks.sort(((TaskItem a, TaskItem b){
+          return a.name.compareTo(b.name);
+        }));
+        return tasks;
+      case OrderTypes.nameZA:
+        tasks.sort(((TaskItem a, TaskItem b){
+          return b.name.compareTo(a.name);
+        }));
+        return tasks;
+      case OrderTypes.addNew:
+        if(serverType==StoreType.qbit){
+          tasks.sort((TaskItem a, TaskItem b){
+            if(a.addTime==null || b.addTime==null){
+              return a.name.compareTo(b.name);
+            }
+            return b.addTime!.compareTo(a.addTime!);
+          });
+          return tasks;
+        }
+        return tasks.reversed.toList();
+      case OrderTypes.addOld:
+        if(serverType==StoreType.qbit){
+          tasks.sort((TaskItem a, TaskItem b){
+            if(a.addTime==null || b.addTime==null){
+              return a.name.compareTo(b.name);
+            }
+            return a.addTime!.compareTo(b.addTime!);
+          });
+          return tasks;
+        }
+        return tasks.toList();
+    }
+  }
+
+  void makeTasks(List<TaskItem> tasks, StoreType serverType){
     if(page.value==Pages.active){
-      activeTasks.value=tasks;
+      activeTasks.value=orderHandler(tasks, activeOrder.value, serverType);
     }else if(page.value==Pages.finish){
-      finishedTask.value=tasks;
+      finishedTask.value=orderHandler(tasks, finishOrder.value, serverType);
     }
   }
 }
