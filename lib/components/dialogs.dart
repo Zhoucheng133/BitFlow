eb533c0cac1d94719cf6ce1b0dc5516fa1e2ace3
zhoucheng <zhoucheng133@outlook.com>
2025-07-18 20:56:52 +0800

    添加关于页面

--- lib/components/dialogs.dart
+++ lib/components/dialogs.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 Future<void> showErrWarnDialog(BuildContext context, String title, String content) async {
   await showDialog(
@@ -16,7 +20,7 @@ Future<void> showErrWarnDialog(BuildContext context, String title, String conten
   );
 }
 
-showComfirmDialog(BuildContext context, String title, String content) async {
+Future<bool> showComfirmDialog(BuildContext context, String title, String content) async {
   bool ok=false;
   await showDialog(
     context: context, 
@@ -39,4 +43,111 @@ showComfirmDialog(BuildContext context, String title, String content) async {
     )
   );
   return ok;
+}
+
+Future<void> showAbout(BuildContext context) async {
+  PackageInfo packageInfo = await PackageInfo.fromPlatform();
+  if(context.mounted){
+    showDialog(
+      context: context, 
+      builder: (BuildContext context)=>AlertDialog(
+        title: Text('关于'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/icon.png',
+              width: 100,
+            ),
+            const SizedBox(height: 10,),
+            Text(
+              'netPlayer',
+              style: GoogleFonts.notoSansSc(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 3,),
+            Text(
+              'Next v${packageInfo.version}',
+              style: GoogleFonts.notoSansSc(
+                fontSize: 13,
+                color: Colors.grey[400]
+              ),
+            ),
+            const SizedBox(height: 20,),
+            GestureDetector(
+              onTap: (){
+                final url=Uri.parse('https://github.com/Zhoucheng133/bitflow');
+                launchUrl(url);
+              },
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const FaIcon(
+                      FontAwesomeIcons.github,
+                      size: 15,
+                    ),
+                    const SizedBox(width: 5,),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 2),
+                      child: Text(
+                        '项目地址',
+                        style: GoogleFonts.notoSansSc(
+                          fontSize: 13,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            GestureDetector(
+              onTap: ()=>showLicensePage(
+                applicationName: 'BitFlow',
+                applicationVersion: 'v${packageInfo.version}',
+                context: context
+              ),
+              child: MouseRegion(
+                cursor: SystemMouseCursors.click,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const FaIcon(
+                      FontAwesomeIcons.certificate,
+                      size: 15,
+                    ),
+                    const SizedBox(width: 5,),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 2),
+                      child: Text(
+                        "许可证",
+                        style: GoogleFonts.notoSansSc(
+                          fontSize: 13,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: (){
+              Navigator.pop(context);
+            }, 
+            child: Text('好的')
+          )
+        ],
+      ),
+    );
+  }
 }
