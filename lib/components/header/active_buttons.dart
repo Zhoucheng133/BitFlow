6927ea0fb323675c4a902ce04e4288c938d2856a
zhoucheng <zhoucheng133@outlook.com>
2025-07-19 19:10:54 +0800

    添加回车添加任务

--- lib/components/header/active_buttons.dart
+++ lib/components/header/active_buttons.dart
@@ -3,6 +3,7 @@ import 'package:bit_flow/components/header/header_button_item.dart';
 import 'package:bit_flow/service/funcs.dart';
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,6 +14,15 @@ class ActiveButtons extends StatefulWidget {
   State<ActiveButtons> createState() => _ActiveButtonsState();
 }
 
+void addTaskHandler(BuildContext context, TextEditingController link, FuncsService funcs){
+  if(!link.text.startsWith("http://") && !link.text.startsWith("https://") && !link.text.startsWith("magnet:?xt=urn:btih:")){
+    showErrWarnDialog(context, "添加任务失败", "链接不合法");
+    return;
+  }
+  funcs.addTaskHandler(link.text);
+  Navigator.pop(context);
+}
+
 Future<void> addTaskDialog(BuildContext context) async {
   final FuncsService funcs=Get.find();
   final TextEditingController link=TextEditingController();
@@ -21,6 +31,10 @@ Future<void> addTaskDialog(BuildContext context) async {
     link.text=copyText;
   }
   if(context.mounted){
+
+    FocusNode node=FocusNode();
+    FocusNode inputNode=FocusNode();
+
     showDialog(
       context: context, 
       builder: (BuildContext context) => AlertDialog(
@@ -28,45 +42,54 @@ Future<void> addTaskDialog(BuildContext context) async {
         content: SizedBox(
           width: 400,
           child: StatefulBuilder(
-            builder: (BuildContext context, setState)=>Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Text('若要添加多个任务，用回车拆分'),
-                const SizedBox(height: 5,),
-                TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'http(s)://\nmagnet:?xt=urn:btih:', 
-                    hintStyle: GoogleFonts.notoSansSc(
-                      color: Colors.grey,
-                      fontSize: 13
-                    ),
-                    isCollapsed: true,
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12)
-                  ),
-                  minLines: 3,
-                  maxLines: null,
-                  controller: link,
-                  style: GoogleFonts.notoSansSc(
-                    fontSize: 13
-                  ),
-                )
-              ],
-            )
+            builder: (BuildContext context, setState){
+              WidgetsBinding.instance.addPostFrameCallback((_) {
+                if (context.mounted) {
+                  FocusScope.of(context).requestFocus(node);
+                }
+              });
+              return KeyboardListener(
+                focusNode: node,
+                onKeyEvent: (event){
+                  if(event is KeyDownEvent && event.logicalKey==LogicalKeyboardKey.enter && !inputNode.hasFocus){
+                    addTaskHandler(context, link, funcs);
+                  }
+                },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text('若要添加多个任务，用回车拆分'),
+                    const SizedBox(height: 5,),
+                    TextField(
+                      focusNode: inputNode,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'http(s)://\nmagnet:?xt=urn:btih:', 
+                        hintStyle: GoogleFonts.notoSansSc(
+                          color: Colors.grey,
+                          fontSize: 13
+                        ),
+                        isCollapsed: true,
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12)
+                      ),
+                      minLines: 3,
+                      maxLines: null,
+                      controller: link,
+                      style: GoogleFonts.notoSansSc(
+                        fontSize: 13
+                      ),
+                    )
+                  ],
+                ),
+              );
+            }
           ),
         ),
         actions: [
           TextButton(onPressed: ()=>Navigator.pop(context), child: Text('取消')),
           ElevatedButton(
-            onPressed: (){
-              if(!link.text.startsWith("http://") && !link.text.startsWith("https://") && !link.text.startsWith("magnet:?xt=urn:btih:")){
-                showErrWarnDialog(context, "添加任务失败", "链接不合法");
-                return;
-              }
-              funcs.addTaskHandler(link.text);
-              Navigator.pop(context);
-            }, 
+            onPressed: ()=>addTaskHandler(context, link, funcs), 
             child: Text('添加')
           )
         ],
