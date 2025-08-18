c93ca9d1f450b961572d591b4e0238489dc1f5f5
zhoucheng <zhoucheng133@outlook.com>
2025-08-18 17:02:07 +0800

    添加移动端AppBar和NavigatorBar

--- lib/mobile/main_view.dart
+++ lib/mobile/main_view.dart
@@ -1,4 +1,6 @@
+import 'package:bit_flow/getx/status_get.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class MainView extends StatefulWidget {
   const MainView({super.key});
@@ -8,11 +10,40 @@ class MainView extends StatefulWidget {
 }
 
 class _MainViewState extends State<MainView> {
+
+  final StatusGet statusGet=Get.find();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Hello World!!!"),
+    return Obx(
+      ()=> Scaffold(
+        appBar: AppBar(
+          title: Text(pageToText(statusGet.page.value)),
+          backgroundColor: Theme.of(context).colorScheme.surfaceContainer,
+        ),
+        bottomNavigationBar: NavigationBar(
+          destinations: [
+            NavigationDestination(
+              icon: Icon(Icons.download_rounded),
+              label: "活跃中",
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.download_done_rounded),
+              label: "已完成"
+            ),
+            NavigationDestination(
+              icon: Icon(Icons.settings_rounded),
+              label: "设置"
+            )
+          ],
+          selectedIndex: statusGet.page.value.index,
+          onDestinationSelected: (int index){
+            statusGet.page.value=Pages.values[index];
+          }
+        ),
+        body: Center(
+          child: Text("Hello World!!!"),
+        ),
       ),
     );
   }
